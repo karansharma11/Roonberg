46019a64cfaeb029dc596e07e49989dd50631b7c
karansharam11 <1111314karan@gmail.com>
2023-11-10 18:16:50 +0530

    chats

--- frontend/src/Screens/ProjectSingleScreen.js
+++ frontend/src/Screens/ProjectSingleScreen.js
@@ -300,7 +300,7 @@ function ProjectSingleScreen() {
                         <>
                           <Card className="chatboxes">
                             <Card.Header>
-                              {categoryData && assignedAgent.categoryId}
+                              { assignedAgent.categoryId}
                             </Card.Header>
                             <Card.Body>
                               <Link to={`/chatWindowScreen/${conversion._id}`}>
@@ -309,7 +309,7 @@ function ProjectSingleScreen() {
                                   type="button"
                                   // onClick={conversionHandler(conversion._id)}
                                 >
-                                  {categoryData && assignedAgent.agentId}
+                                  {assignedAgent.agentId}
                                 </Button>
                               </Link>
                             </Card.Body>
