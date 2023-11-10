ba2b7b475d1f58ee53c44bed7c536429c75601fa
karansharam11 <1111314karan@gmail.com>
2023-11-10 17:30:41 +0530

    remove rte

--- frontend/src/Screens/ChatWindowScreen.js
+++ frontend/src/Screens/ChatWindowScreen.js
@@ -24,7 +24,7 @@ import { ColorRing, ThreeDots } from "react-loader-spinner";
 import Button from "react-bootstrap/Button";
 import { Editor } from "@tinymce/tinymce-react";
 // import { EditorValue } from "react-rte";
-import MUIRichTextEditor from 'mui-rte';
+// import MUIRichTextEditor from 'mui-rte';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { MdCancel } from "react-icons/md";
 
